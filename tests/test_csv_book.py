82199b2f81b442f469538fe8a853d5d071458bad
chfw <wangc_2011@hotmail.com>
2015-05-08 09:30:45 +0100

    more unit tests on csvbook.py. coverage 86%

--- tests/test_csv_book.py
+++ tests/test_csv_book.py
@@ -1,7 +1,7 @@
 import os
 from textwrap import dedent
 from nose.tools import raises
-from pyexcel_io import NamedContent, get_io
+from pyexcel_io import NamedContent, get_io, OrderedDict
 from pyexcel_io.csvbook import (
     CSVSheetReader,
     CSVFileReader,
@@ -70,11 +70,10 @@ class TestReadMultipleSheets:
             ["4", "5", "6"],
             ["7", "8", "9"]
         ]
-        self.sheets = {
-            "sheet1": self.data,
-            "sheet2": self.data,
-            "sheet3": self.data
-        }
+        self.sheets = OrderedDict()
+        self.sheets.update({"sheet1": self.data})
+        self.sheets.update({"sheet2": self.data})
+        self.sheets.update({"sheet3": self.data})
         index = 0
         for key, value in self.sheets.items():
             file_name = self.test_file_formatter % (key, index)
@@ -87,6 +86,72 @@ class TestReadMultipleSheets:
         b = CSVBook("csv_multiple.csv")
         sheets = b.sheets()
         assert sheets == self.sheets
+
+    def tearDown(self):
+        index = 0
+        for key, value in self.sheets.items():
+            file_name = self.test_file_formatter % (key, index)
+            os.unlink(file_name)
+            index = index + 1
+
+
+class TestWriteMultipleSheets:
+    def setUp(self):
+        self.file_type = "csv"
+        self.test_file_formatter = "csv_multiple__%s__%s." + self.file_type
+        self.data1 = [
+            ["1", "2", "3"],
+            ["4", "5", "6"],
+            ["7", "8", "9"]
+        ]
+        self.data2 = [
+            ["1", "2", "3"],
+            ["4", "5", "6"],
+            ["7", "8", "1000"]
+        ]
+        self.data3 = [
+            ["1", "2", "3"],
+            ["4", "5", "6888"],
+            ["7", "8", "9"]
+        ]
+        self.sheets = OrderedDict()
+        self.sheets.update({"sheet1": self.data1})
+        self.sheets.update({"sheet2": self.data2})
+        self.sheets.update({"sheet3": self.data3})
+        self.result_dict = OrderedDict()        
+        self.result1 = dedent("""
+           1,2,3
+           4,5,6
+           7,8,9
+        """).strip('\n')
+        self.result2 = dedent("""
+           1,2,3
+           4,5,6
+           7,8,1000
+        """).strip('\n')
+        self.result3 = dedent("""
+           1,2,3
+           4,5,6888
+           7,8,9
+        """).strip('\n')
+        self.result_dict.update({"sheet1": self.result1})
+        self.result_dict.update({"sheet2": self.result2})
+        self.result_dict.update({"sheet3": self.result3})
+
+    def test_multiple_sheet(self):
+        b = CSVWriter("csv_multiple.csv")
+        for key, value in self.sheets.items():
+            w = b.create_sheet(key)
+            w.write_array(value)
+            w.close()
+        b.close()
+        index = 0
+        for key, value in self.sheets.items():
+            file_name = self.test_file_formatter % (key, index)
+            with open(file_name, 'r') as f:
+                content = f.read()
+                assert content.strip('\n') == self.result_dict[key]
+            index = index + 1
 
     def tearDown(self):
         index = 0
@@ -109,7 +174,7 @@ class TestWriter:
            1,2,3
            4,5,6
            7,8,9
-        """).strip('\n') + '\n' # have a trailing newline
+        """).strip('\n')
 
     def test_sheet_writer(self):
         w = CSVSheetWriter(self.test_file, None)
@@ -118,7 +183,7 @@ class TestWriter:
         w.close()
         with open(self.test_file, 'r') as f:
             content = f.read()
-            assert content == self.result
+            assert content.strip('\n') == self.result
     
     def test_book_writer(self):
         b = CSVWriter(self.test_file)
@@ -129,7 +194,43 @@ class TestWriter:
         b.close()
         with open(self.test_file, 'r') as f:
             content = f.read()
-            assert content == self.result
+            assert content.strip('\n') == self.result
     
     def tearDown(self):
         os.unlink(self.test_file)
+
+
+class TestMemoryWriter:
+    def setUp(self):
+        self.file_type = "csv"
+        self.test_file = "csv_book." + self.file_type
+        self.data = [
+            ["1", "2", "3"],
+            ["4", "5", "6"],
+            ["7", "8", "9"]
+        ]
+        self.result = dedent("""
+           1,2,3
+           4,5,6
+           7,8,9
+        """).strip('\n')
+
+    def test_sheet_writer_to_memory(self):
+        io = get_io(self.file_type)
+        w = CSVSheetWriter(io, None)
+        for row in self.data:
+            w.write_row(row)
+        w.close()
+        content = io.getvalue().replace('\r', '')
+        assert content.strip('\n') == self.result
+    
+    def test_book_writer_to_memroy(self):
+        io = get_io(self.file_type)
+        b = CSVWriter(io)
+        w = b.create_sheet(None)
+        for row in self.data:
+            w.write_row(row)
+        w.close()
+        b.close()
+        content = io.getvalue().replace('\r', '')
+        assert content.strip('\n') == self.result
