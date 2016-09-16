f613bd21d527e1a795875315b770d60045d38d8c
chfw <wangc_2011@hotmail.com>
2016-09-16 09:43:48 +0100

    kick start 0.2.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ PY26 = PY2 and sys.version_info[1] < 7
 
 NAME = 'pyexcel-io'
 AUTHOR = 'C.W.'
-VERSION = '0.2.3'
+VERSION = '0.2.4'
 EMAIL = 'wangc_2011 (at) hotmail.com'
 LICENSE = 'New BSD'
 DESCRIPTION = (
