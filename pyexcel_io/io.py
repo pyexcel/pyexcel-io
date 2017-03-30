682562ecb6a544e1c387d14992a93900637def44
chfw <wangc_2011@hotmail.com>
2017-03-30 13:21:31 +0100

    update function doc

--- pyexcel_io/io.py
+++ pyexcel_io/io.py
@@ -15,11 +15,20 @@ import pyexcel_io.constants as constants
 def get_data(afile, file_type=None, streaming=False, **keywords):
     """Get data from an excel file source
 
-    :param filename: actual file name, a file stream or actual content
+    :param afile: a file name, a file stream or actual content
     :param sheet_name: the name of the sheet to be loaded
     :param sheet_index: the index of the sheet to be loaded
     :param file_type: used only when filename is not a physial file name
-    :param keywords: any other parameters
+    :param streaming: toggles the type of returned data. The values of the
+                      returned dictionary remain as generator if it is set
+                      to True. Default is False.
+    :param library: explicitly name a library for use.
+                    e.g. library='pyexcel-ods'
+    :param auto_detect_float: defaults to True
+    :param auto_detect_int: defaults to True
+    :param auto_detect_datetime: defaults to True
+    :param ignore_infinity: defaults to True
+    :param keywords: any other library specific parameters
     :returns: an ordered dictionary
     """
     if isstream(afile) and file_type is None:
@@ -48,10 +57,11 @@ def save_data(afile, data, file_type=None, **keywords):
     :param filename: actual file name, a file stream or actual content
     :param data: a dictionary but an ordered dictionary is preferred
     :param file_type: used only when filename is not a physial file name
+    :param library: explicitly name a library for use.
+                    e.g. library='pyexcel-ods'
     :param keywords: any other parameters that python csv module's
-                     `fmtparams <https://docs.python.org/release/3.1.5/
-                      library/csv.html#dialects-and-formatting-parameters>`_
-    """
+                     `fmtparams <https://docs.python.org/release/3.1.5/library/csv.html#dialects-and-formatting-parameters>`_
+    """  # noqa
     to_store = data
     if isinstance(data, list) or is_generator(data):
         single_sheet_in_book = True
