60d375b25dd5494d2587cab0ddf9fbcf905f09a7
chfw <wangc_2011@hotmail.com>
2017-11-10 17:12:15 +0000

    :bug: make UnicodeWriter to work with python 3

--- pyexcel_io/writers/csvw.py
+++ pyexcel_io/writers/csvw.py
@@ -35,7 +35,8 @@ class UnicodeWriter(object):
                               for s in row])
         # Fetch UTF-8 output from the queue ...
         data = self.queue.getvalue()
-        data = data.decode("utf-8")
+        if compact.PY2:
+            data = data.decode("utf-8")
         # ... and reencode it into the target encoding
         data = self.encoder.encode(data)
         # write to the target stream
