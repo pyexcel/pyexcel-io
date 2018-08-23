ff5f77de6e3e0822d2d26936e7ec95a61c2e0a2d
chfw <wangc_2011@hotmail.com>
2018-08-23 19:09:07 +0100

    :books: update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ author = u'C.W.'
 # The short X.Y version
 version = u'0.5.8'
 # The full version, including alpha/beta/rc tags
-release = u'0.5.8'
+release = u'0.5.9'
 
 
 # -- General configuration ---------------------------------------------------
