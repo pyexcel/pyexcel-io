1dfe9457f19aeb4b1e1f3548be00e99526dd4ca2
chfw <wangc_2011@hotmail.com>
2019-03-19 07:48:40 +0000

    :green_heart: update unit test

--- tests/test_service.py
+++ tests/test_service.py
@@ -100,7 +100,7 @@ def test_detect_float_value_on_custom_nan_text2():
 def test_ods_write_format_conversion():
     if PY2:
         expected = ODS_WRITE_FORMAT_COVERSION[long]  # noqa: F821
-        eq_('throw_exception', expected)
+        eq_('long', expected)
     else:
         raise SkipTest()
 
