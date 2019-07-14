84ec6199fbdbb0555ca13106d2a79733c1e88ae2
chfw <wangc_2011@hotmail.com>
2019-07-14 08:17:35 +0100

    :books: update docs and prepare for an incremental release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,9 @@ project = 'pyexcel-io'
 copyright = '2015-2019 Onni Software Ltd.'
 author = 'C.W.'
 # The short X.Y version
-version = '0.5.18'
+version = '0.5.19'
 # The full version, including alpha/beta/rc tags
-release = '0.5.18'
+release = '0.5.19'
 
 # -- General configuration ---------------------------------------------------
 
