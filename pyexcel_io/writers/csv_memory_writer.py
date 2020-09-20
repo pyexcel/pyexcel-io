4b56cfcf1e8c46b8a9204ee70fcc0dcb3655e177
chfw <wangc_2011@hotmail.com>
2020-09-20 12:28:21 +0100

    :horse: prototype

---
+++ pyexcel_io/writers/csv_memory_writer.py
@@ -0,0 +1,12 @@
+class CsvMemoryWriter:
+    def open(self, file_name, **keywords):
+        pass
+
+    def write(self, a_dict):
+        pass
+
+    def create_sheet(self, sheet_name):
+        pass
+
+    def close(self):
+        pass
