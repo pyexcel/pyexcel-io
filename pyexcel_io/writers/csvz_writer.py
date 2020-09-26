787da44396257db2e943edceddb8e161f552980c
chfw <wangc_2011@hotmail.com>
2020-09-26 14:47:53 +0100

    :hammer: interface update

--- pyexcel_io/writers/csvz_writer.py
+++ pyexcel_io/writers/csvz_writer.py
@@ -27,6 +27,15 @@ class CsvZipWriter(object):
         )
         return writer
 
+    def write(self, incoming_dict):
+        for sheet_name in incoming_dict:
+            sheet_writer = self.create_sheet(sheet_name)
+            if sheet_writer:
+                sheet_writer.write_array(incoming_dict[sheet_name])
+                sheet_writer.close()
+            else:
+                raise Exception("Cannot create a sheet writer!")
+
     def close(self):
         if self.zipfile:
             self.zipfile.close()
