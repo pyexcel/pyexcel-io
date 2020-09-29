6ec89c4612ee0d91af1dbbc9caed5fa8fdad5c1f
chfw <wangc_2011@hotmail.com>
2020-09-29 22:20:52 +0100

    :tada: update available plugins

--- pyexcel_io/utils.py
+++ pyexcel_io/utils.py
@@ -13,7 +13,13 @@ XLS_PLUGIN = "pyexcel-xls"
 XLSX_PLUGIN = "pyexcel-xlsx"
 ODS_PLUGIN = "pyexcel-ods"
 ODS3_PLUGIN = "pyexcel-ods3"
+ODSR_PLUGIN = "pyexcel-odsr"
+ODSW_PLUGIN = "pyexcel-odsw"
+XLSXR_PLUGIN = "pyexcel-xlsxr"
 XLSXW_PLUGIN = "pyexcel-xlsxw"
+XLSBR_PLUGIN = "pyexcel-xlsbr"
+HTMLR_PLUGIN = "pyexcel-htmlr"
+PDFR_PLUGIN = "pyexcel-pdfr"
 IO_ITSELF = "pyexcel-io"
 
 AVAILABLE_NEW_READERS = {}
@@ -23,17 +29,20 @@ AVAILABLE_READERS = {
     constants.FILE_FORMAT_XLS: [XLS_PLUGIN],
     constants.FILE_FORMAT_XLSX: [XLS_PLUGIN, XLSX_PLUGIN],
     constants.FILE_FORMAT_XLSM: [XLS_PLUGIN, XLSX_PLUGIN],
-    constants.FILE_FORMAT_ODS: [ODS_PLUGIN, ODS3_PLUGIN],
+    constants.FILE_FORMAT_ODS: [ODS_PLUGIN, ODS3_PLUGIN, ODSR_PLUGIN],
     constants.FILE_FORMAT_TSV: [IO_ITSELF],
     constants.FILE_FORMAT_CSVZ: [IO_ITSELF],
     constants.FILE_FORMAT_TSVZ: [IO_ITSELF],
+    constants.FILE_FORMAT_XLSB: [XLSBR_PLUGIN],
+    constants.FILE_FORMAT_HTML: [HTMLR_PLUGIN],
+    constants.FILE_FORMAT_PDF: [PDFR_PLUGIN],
 }
 
 AVAILABLE_WRITERS = {
     constants.FILE_FORMAT_XLS: [XLS_PLUGIN],
     constants.FILE_FORMAT_XLSX: [XLSX_PLUGIN, XLSXW_PLUGIN],
     constants.FILE_FORMAT_XLSM: [XLSX_PLUGIN],
-    constants.FILE_FORMAT_ODS: [ODS_PLUGIN, ODS3_PLUGIN],
+    constants.FILE_FORMAT_ODS: [ODS_PLUGIN, ODS3_PLUGIN, ODSW_PLUGIN],
     constants.FILE_FORMAT_CSV: [IO_ITSELF],
     constants.FILE_FORMAT_TSV: [IO_ITSELF],
     constants.FILE_FORMAT_CSVZ: [IO_ITSELF],
