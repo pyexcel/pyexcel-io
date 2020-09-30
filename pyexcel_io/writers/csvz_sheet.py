50026d690d87fc9c5e170a85ce7c26576e59dda3
chfw <wangc_2011@hotmail.com>
2020-09-30 22:05:52 +0100

    :sparkles: outline the plugin sheet writer interface. well, break the old interface

--- pyexcel_io/writers/csvz_sheet.py
+++ pyexcel_io/writers/csvz_sheet.py
@@ -7,23 +7,23 @@
     :copyright: (c) 2014-2020 by Onni Software Ltd.
     :license: New BSD License, see LICENSE for more details
 """
+import csv
+
 from pyexcel_io._compact import StringIO
-from pyexcel_io.writers.csv_sheet import CSVSheetWriter
+from pyexcel_io.writers.csv_sheet import CSVFileWriter
 
 
-class CSVZipSheetWriter(CSVSheetWriter):
+class CSVZipSheetWriter(CSVFileWriter):
     """ handle the zipfile interface """
 
     def __init__(self, zipfile, sheetname, file_extension, **keywords):
         self.file_extension = file_extension
         keywords["single_sheet_in_book"] = False
-        CSVSheetWriter.__init__(self, zipfile, sheetname, **keywords)
-
-    def set_sheet_name(self, name):
         self.content = StringIO()
-        import csv
+        super().__init__(zipfile, sheetname, **keywords)
 
-        self.writer = csv.writer(self.content, **self._keywords)
+    def get_writer(self):
+        return csv.writer(self.content, **self._keywords)
 
     def close(self):
         file_name = "%s.%s" % (self._native_sheet, self.file_extension)
