66906c4e230bffe063e3e5e6c732dea929107ec9
chfw <wangc_2011@hotmail.com>
2020-12-05 22:43:12 +0000

    :books: update next version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ except (ValueError, UnicodeError, locale.Error):
 
 NAME = "pyexcel-io"
 AUTHOR = "C.W."
-VERSION = "0.6.5"
+VERSION = "0.6.4"
 EMAIL = "info@pyexcel.org"
 LICENSE = "New BSD"
 DESCRIPTION = (
