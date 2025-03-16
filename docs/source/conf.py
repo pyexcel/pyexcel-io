62145630e9fb15636ee6d043c7446c08a83e9e69
chfw <wangc_2011@hotmail.com>
2025-03-16 11:35:12 +0000

    use lml 0.2.0 for test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -69,7 +69,8 @@ html_static_path = ['_static']
 # -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/3/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/3',
+                                  'python-inv.txt')}
 # TODO: html_theme not configurable upstream
 html_theme = 'default'
 
